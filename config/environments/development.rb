f90229c868e669d1f1be92a98a7a7bc682699ac8
Shaqri <peter200970@hotmail.com>
2020-09-23 11:50:56 -0500

    install aplication gems and dependecies

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # Devise-default-url
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
